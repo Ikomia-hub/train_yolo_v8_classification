f681e2a7744aeb2f33bf088643b1bf7f2155d7ad
Allan Kouidri <87222273+allankouidri@users.noreply.github.com>
2023-11-16 14:59:12 +0100

    patch version: fix model download with ultralytics v8.0.117

--- train_yolo_v8_classification_process.py
+++ train_yolo_v8_classification_process.py
@@ -187,7 +187,7 @@ class TrainYoloV8ClassificationFactory(dataprocess.CTaskFactory):
         self.info.short_description = "Train YOLOv8 classification models."
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Classification"
-        self.info.version = "1.0.1"
+        self.info.version = "1.0.2"
         self.info.icon_path = "icons/icon.png"
         self.info.authors = "Jocher, G., Chaurasia, A., & Qiu, J"
         self.info.article = "YOLO by Ultralytics"
